654ef416999d88c600ae5ec17bd85ce7cc2b6ac9
Brian <gamerfunky78@gmail.com>
2024-10-02 16:39:37 +0300

    remove collumns due to functionality lack and since its unusable in the project either way. //also since i have a really bad network i couldnt test the functionality itself which was a mailing one that i made in a separate project.

--- src/collumns.jsx
+++ src/collumns.jsx
@@ -2,22 +2,7 @@ export default function Collumns(){
 
     return(
         <>
-        <div className="MC flex flex-row gap-5" id="MC">
-
         
-            <div className="collumns1 flex flex-col ">
-                    <div className="ball bg-black dark:bg-white">
-                        <button>.</button>
-                    </div>
-                    <div className="C1 flex flex-col bg-black dark:bg-white">
-                        <p>.</p>
-                    </div >
-            </div>
-
-            <div className="Hire bg-black dark:bg-white">
-                <p className="text-white dark:text-black">HIRE ME</p>
-            </div>
-        </div>
         </>
 
         
