572275f538a0c19171086a6c9775ebf1ceed51bc
Brian <gamerfunky78@gmail.com>
2024-10-03 13:58:55 +0300

    added little sequence follow up for images for better animations.

--- src/about.jsx
+++ src/about.jsx
@@ -108,16 +108,60 @@ export default function About() {
                         viewport={{ once: true }}
                     
                     >
-                        <div className="flex flex-row gap-3" id="BADGES">
-                        <img src="https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB" alt="" />
-                        <img src="https://img.shields.io/badge/tailwindcss-%2338B2AC.svg?style=for-the-badge&logo=tailwind-css&logoColor=white" alt="" />
-                        <img src="https://img.shields.io/badge/vite-%23646CFF.svg?style=for-the-badge&logo=vite&logoColor=white" alt="" />
-                        <img src="https://img.shields.io/badge/node.js-6DA55F?style=for-the-badge&logo=node.js&logoColor=white" alt="" />
-                        <img src="https://img.shields.io/badge/MUI-%230081CB.svg?style=for-the-badge&logo=mui&logoColor=white" alt="" />
+                    <div className="flex flex-row gap-3" id="BADGES">
+
+                        <motion.img 
+                            initial={{ opacity: 0, y: 10 }} // Starting position
+                            whileInView={{ opacity: 1, y: 0 }} // Ending position when in view
+                            transition={{ duration: 1}} // Transition duration
+                            viewport={{ once: true }}src="https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB" alt="" />
+                        <motion.img 
+                            initial={{ opacity: 0, y: 20 }} // Starting position
+                            whileInView={{ opacity: 1, y: 0 }} // Ending position when in view
+                            transition={{ duration: 1}} // Transition duration
+                            viewport={{ once: true }} src="https://img.shields.io/badge/tailwindcss-%2338B2AC.svg?style=for-the-badge&logo=tailwind-css&logoColor=white" alt="" />
+                        <motion.img 
+                            initial={{ opacity: 0, y: 30 }} // Starting position
+                            whileInView={{ opacity: 1, y: 0 }} // Ending position when in view
+                            transition={{ duration: 1}} // Transition duration
+                            viewport={{ once: true }} src="https://img.shields.io/badge/vite-%23646CFF.svg?style=for-the-badge&logo=vite&logoColor=white" alt="" />
+                        <motion.img 
+                            initial={{ opacity: 0, y: 40 }} // Starting position
+                            whileInView={{ opacity: 1, y: 0 }} // Ending position when in view
+                            transition={{ duration: 1}} // Transition duration
+                            viewport={{ once: true }}src="https://img.shields.io/badge/node.js-6DA55F?style=for-the-badge&logo=node.js&logoColor=white" alt="" />
+                        <motion.img 
+                            initial={{ opacity: 0, y: 50 }} // Starting position
+                            whileInView={{ opacity: 1, y: 0 }} // Ending position when in view
+                            transition={{ duration: 1}} // Transition duration
+                            viewport={{ once: true }} src="https://img.shields.io/badge/MUI-%230081CB.svg?style=for-the-badge&logo=mui&logoColor=white" alt="" />
+                        <motion.img 
+                            initial={{ opacity: 0, y: 60 }} // Starting position
+                            whileInView={{ opacity: 1, y: 0 }} // Ending position when in view
+                            transition={{ duration: 1}} // Transition duration
+                            viewport={{ once: true }} src="https://shields.io/badge/TypeScript-3178C6?logo=TypeScript&logoColor=FFF&style=flat-square" alt="" />
+                    
                     </div>
 
                     </motion.div>
                     <br />
+                    <motion.div
+                        initial={{ opacity: 0, y: 70 }} // Starting position
+                        whileInView={{ opacity: 1, y: 0 }} // Ending position when in view
+                        transition={{ duration: 0.9 }} // Transition duration
+                        viewport={{ once: true}} // Animation happens only once when it comes into view
+                    >
+                        <motion.p
+                            className='Desc'
+                            initial={{ opacity: 0, y: 70 }} // Starting position
+                            whileInView={{ opacity: 1, y: 0 }} // Ending position when in view
+                            transition={{ duration: 0.3 }} // Transition duration
+                            viewport={{ once: true}} // Animation happens only once when it comes into view
+                        >
+                            Iam also Consistent as i aspire to make better applications here are my stats:
+                        </motion.p>
+                        <img src="https://github-readme-stats.vercel.app/api?username=gamerfunky78&theme=merko&show_icons=true&hide_border=true&count_private=true" alt="" />
+                    </motion.div>
                     <motion.div 
                         className="bg-transparent flex flex-col justify-center items-center"
                         initial={{ opacity: 0, y: 70 }} // Starting position
