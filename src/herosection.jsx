ee2311bf387618c6599e266f7f86348554addcbc
Brian <gamerfunky78@gmail.com>
2025-06-17 13:55:09 +0300

    update hero-section

--- src/herosection.jsx
+++ src/herosection.jsx
@@ -147,7 +147,7 @@ export default function Herosection() {
 
 
             </div>
-            <div className="absolute bottom-20 left-1/2 transform -translate-x-1/2">
+            <div className="absolute bottom-20 left-1/2 transform -translate-x-1/2" id="ScrollIndicator" >
                 <a href="#ABOUT">
                     <ScrollDownIndicator />
                 </a>
